96dbb20b13e0c23ef68627cb081856ccf8950f13
Debuggindan <raphealdaniel14@yahoo.com>
2023-02-20 08:00:54 -0800

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - returns outputed numbers
+  * Return: Return 0 (Success)
+  */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+		putchar('\n');
+		return (0);
+}
+
